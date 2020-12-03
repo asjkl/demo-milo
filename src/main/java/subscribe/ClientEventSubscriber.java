eee7821347ebb16872dcb16414d5db4d7f7b6746
giovanni.iannuzzi <giovanni.iannuzzi@OCTANS01.tezar.bogus>
2020-12-03 09:36:27 +0100

    Event subscriber esternalizzato

--- src/main/java/subscribe/ClientEventSubscriber.java
+++ src/main/java/subscribe/ClientEventSubscriber.java
@@ -9,30 +9,36 @@ import org.eclipse.milo.opcua.sdk.client.subscriptions.ManagedDataItem;
 import org.eclipse.milo.opcua.sdk.client.subscriptions.ManagedEventItem;
 import org.eclipse.milo.opcua.sdk.client.subscriptions.ManagedSubscription;
 import org.eclipse.milo.opcua.sdk.client.subscriptions.ManagedSubscription.ChangeListener;
+import org.eclipse.milo.opcua.stack.core.AttributeId;
+import org.eclipse.milo.opcua.stack.core.Identifiers;
 import org.eclipse.milo.opcua.stack.core.UaException;
 import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
 import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.QualifiedName;
 import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
 import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.UInteger;
 import org.eclipse.milo.opcua.stack.core.types.enumerated.MonitoringMode;
+import org.eclipse.milo.opcua.stack.core.types.structured.ContentFilter;
+import org.eclipse.milo.opcua.stack.core.types.structured.EventFilter;
 import org.eclipse.milo.opcua.stack.core.types.structured.ModifySubscriptionResponse;
+import org.eclipse.milo.opcua.stack.core.types.structured.SimpleAttributeOperand;
 
 import client.SynchronousClient;
 import utils.Constants;
 
-public class ClientSubscriber implements Runnable {
+public class ClientEventSubscriber implements Runnable {
     
     private Boolean exit = false;
     private ManagedSubscription subscription;
     private OpcUaClient opcUaClient;
     private Integer namespace =2;
-    private String nodeId="HelloWorld/ScalarTypes/Int32";
+    private String nodeId="HelloWorld";
     
     //Il tempo (espresso in ms) che passa prima che il subscriber richieda eventuali aggiornamenti al server
-    private Integer publishingInterval= 5000;
+    private Integer publishingInterval= 0;
     
     //Il tempo (espresso in ms) che passa prima che il server controlli eventuali cambiamenti dell'elemento da monitorare
-    private Double samplingInterval= 3000.0;
+    private Double samplingInterval= 0.0;
     
     //La dimensione della coda rappresenta quanti cambiamenti di valore possono essere salvati prima di applicare una policy di discard
     private Integer queueSize= 5;
@@ -45,7 +51,7 @@ public class ClientSubscriber implements Runnable {
     //se settato a DISABLED il server non effettua alcuna operazione
     private MonitoringMode monitoringMode=MonitoringMode.Reporting;
     
-    public ClientSubscriber(OpcUaClient opcUaClient) throws UaException {
+    public ClientEventSubscriber(OpcUaClient opcUaClient) throws UaException {
 	this.opcUaClient=opcUaClient;
 	
 	//Creiamo la configurazione necessaria al monitoraggio
@@ -58,27 +64,59 @@ public class ClientSubscriber implements Runnable {
 	//il secondo parametro è opzionale ed indica quanto tempo bisogna attendere per richiedere l'aggiornamento dell'elemento
 	subscription = ManagedSubscription.create(opcUaClient, publishingInterval);
 	
-	//Aggiungiamo al gestore di sottoscrizioni l'elemento da monitorare
-	ManagedDataItem dataItem = subscription.createDataItem(new NodeId(namespace,nodeId ));
-	dataItem.setSamplingInterval(samplingInterval);
-	dataItem.setQueueSize(UInteger.valueOf(queueSize));
-	dataItem.setDiscardOldest(discardOldest);
-	dataItem.setMonitoringMode(monitoringMode);
+	EventFilter eventFilter = new EventFilter(
+		    new SimpleAttributeOperand[]{
+		        new SimpleAttributeOperand(
+		            Identifiers.BaseEventType,
+		            new QualifiedName[]{new QualifiedName(0, "EventId")},
+		            AttributeId.Value.uid(),
+		            null),
+		        new SimpleAttributeOperand(
+		                    Identifiers.BaseEventType,
+		                    new QualifiedName[]{new QualifiedName(0, "EventType")},
+		                    AttributeId.Value.uid(),
+		                    null),
+		        new SimpleAttributeOperand(
+		                    Identifiers.BaseEventType,
+		                    new QualifiedName[]{new QualifiedName(0, "SourceName")},
+		                    AttributeId.Value.uid(),
+		                    null),
+		        new SimpleAttributeOperand(
+		                    Identifiers.BaseEventType,
+		                    new QualifiedName[]{new QualifiedName(0, "Severity")},
+		                    AttributeId.Value.uid(),
+		                    null),
+		        new SimpleAttributeOperand(
+		            Identifiers.BaseEventType,
+		            new QualifiedName[]{new QualifiedName(0, "Time")},
+		            AttributeId.Value.uid(),
+		            null),
+		        new SimpleAttributeOperand(
+		            Identifiers.BaseEventType,
+		            new QualifiedName[]{new QualifiedName(0, "Message")},
+		            AttributeId.Value.uid(),
+		            null)
+		    },
+		    new ContentFilter(null)
+		);
 	
+	ManagedEventItem eventItem = subscription.createEventItem(Identifiers.Server, eventFilter);
+		
 	//Aggiungiamo al gestore di sottoscrizioni le reazioni a determinati eventi
 	subscription.addChangeListener(new ChangeListener() {
-	    	
-	    	//Quando l'elemento (o gli elementi) da monitorare cambiano e passa il tempo di pulling
-	    	//stampiamo il valore aggiornato
-    	        @Override
-    	        public void onDataReceived(List<ManagedDataItem> dataItems, List<DataValue> dataValues) {
-    	            System.out.println("->"+dataValues.get(0).getValue().getValue());           
-    	            if(dataValues.get(0).getValue().getValue().equals(-1)) {
-    	        	exit = true;
-    	            }
-    	        }
     	        
+    	        @Override
+    		public void onEventReceived(List<ManagedEventItem> eventItems, List<Variant[]> eventFields) {
+    	                for(Variant[] event : eventFields) {
+    	                    for(Variant v: event) {
+    	                	System.out.println(v.getValue());
+    	                    }
+    	                System.out.println();
+    	                }
+    	                System.out.println("----------");
+    		}
     	    });
+
     }
 
 
@@ -89,7 +127,7 @@ public class ClientSubscriber implements Runnable {
 	OpcUaClient opcUaClient = SynchronousClient.connect(endpoint);
 	
 	//Viene istanziato il thread e fatto partire
-	ClientSubscriber subscriptionThread =new ClientSubscriber(opcUaClient);
+	ClientEventSubscriber subscriptionThread =new ClientEventSubscriber(opcUaClient);
 	subscriptionThread.run();
 	
     }
