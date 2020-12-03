d33f07d1d602739fade0ddde39fbce309258d097
Giovanni <Giovanni@192.168.114.15>
2020-12-03 16:33:22 +0100

    Server opcuaserver (reader & subscriber)

---
+++ src/main/java/external/server/opcuaserver/Reader.java
@@ -0,0 +1,105 @@
+package external.server.opcuaserver;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+import org.eclipse.milo.opcua.sdk.client.AddressSpace;
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.nodes.UaNode;
+import org.eclipse.milo.opcua.sdk.client.nodes.UaVariableNode;
+import org.eclipse.milo.opcua.sdk.core.AccessLevel;
+import org.eclipse.milo.opcua.stack.core.Identifiers;
+import org.eclipse.milo.opcua.stack.core.UaException;
+import org.eclipse.milo.opcua.stack.core.types.builtin.ByteString;
+import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.NodeClass;
+
+import client.SynchronousClient;
+import utils.Constants;
+
+public class Reader {
+    private static Integer namespace =3;
+    private static String nodeId = "AirConditioner_1";
+ 
+    public static void main(String[] args) throws UaException, InterruptedException, ExecutionException {
+	final String endpoint = String.format("opc.tcp://%s:%s%s", "opcuaserver.com", 48010, "/test");
+
+	
+	Reader reader = new Reader();
+	
+	//Creiamo una connessione sincrona con il server OPC
+	OpcUaClient opcUaClient = SynchronousClient.connect(endpoint);
+	
+	System.out.println("Connect");
+	
+	String spazio="";
+	
+	//Ricaviamo l'address space che contiene tutti i nodi del server
+	AddressSpace addressSpace = opcUaClient.getAddressSpace();
+	
+	//Dal address space prendiamo il nodo da cui vogliamo partire (per esempio nodo radice)
+	NodeId node = new NodeId(namespace, nodeId);
+	UaNode serverNode = addressSpace.getNode(new NodeId(Unsigned.ushort(namespace), nodeId));				// new NodeId(Unsigned.ushort(namespace), nodeId)
+	
+	System.out.format("%-60s %-15s %-15s %-15s %-15s %-15s%n", "NodeName", "NodeType","NodeWritable", "NameSpaceIndex", "NodeId", "Value");
+	System.out.println("-----------------------------------------------------------------------------------------------------------------");
+	reader.stampa(serverNode, spazio);
+	
+	//Esploriamo l'address space a partire dal nodo scelto
+	reader.broswe(serverNode, addressSpace, spazio);
+	
+	opcUaClient.disconnect();
+	
+
+    }
+    
+    //Ricorsivamente si vanno ad esplorare tutti i nodi figli partendo dal nodo padre
+    public void broswe(UaNode nodo, AddressSpace addressSpace, String spazio) throws UaException {
+	List<UaNode> nodes = (List<UaNode>) addressSpace.browseNodes(nodo);
+	    
+	    for (UaNode uaNode : nodes) {
+		stampa(uaNode, spazio);
+		broswe(uaNode, addressSpace, spazio+" ");
+	    }
+    }
+    
+    public void stampa(UaNode node, String spazio) throws UaException {
+	String nodeName = node.getBrowseName().getName().toString();
+	String nodeType = node.getNodeClass().toString();
+	Boolean nodeWritable = false;
+	String nameSpaceIndex = node.getBrowseName().getNamespaceIndex().toString();
+	String nodeId = node.getNodeId().getIdentifier().toString();
+	Object value = null;
+
+	//Se il nodo è di tipo variabile possiamo recuperare il valore
+	if(node.getNodeClass().equals(NodeClass.Variable)) {
+	   
+	    //Lettura del valore di una variabile
+    	    value = ((UaVariableNode)node).readValue().getValue().getValue();
+    	   
+    	    //Controllo se è possibile scrivere la variabile, è possibile fare lo stesso per la lettura
+    	    Set<AccessLevel> accessLevel = AccessLevel.fromValue(((UaVariableNode)node).getAccessLevel());
+    	    nodeWritable=accessLevel.contains(AccessLevel.CurrentWrite);
+    	   
+    	    System.out.format("%-60s %-15s %-15s %-15s %-15s %-15s%n", 
+    		    spazio+nodeName,
+    		    nodeType,
+    		    nodeWritable,
+    		    nameSpaceIndex, 
+    		    nodeId,
+    		    value);
+    	    
+         }else {
+    	    System.out.format("%-60s %-15s %-15s %-15s %-15s %-15s%n", 
+    		    spazio+nodeName,
+    		    nodeType,
+    		    nodeWritable, 
+    		    nameSpaceIndex,
+    		    nodeId,
+    		    "-");
+    	}
+    }
+}
---
+++ src/main/java/external/server/opcuaserver/Subscriber.java
@@ -0,0 +1,119 @@
+package external.server.opcuaserver;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.bouncycastle.asn1.eac.UnsignedInteger;
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.subscriptions.ManagedDataItem;
+import org.eclipse.milo.opcua.sdk.client.subscriptions.ManagedEventItem;
+import org.eclipse.milo.opcua.sdk.client.subscriptions.ManagedSubscription;
+import org.eclipse.milo.opcua.sdk.client.subscriptions.ManagedSubscription.ChangeListener;
+import org.eclipse.milo.opcua.stack.core.UaException;
+import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
+import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
+import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.UInteger;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.MonitoringMode;
+import org.eclipse.milo.opcua.stack.core.types.structured.ModifySubscriptionResponse;
+
+import client.SynchronousClient;
+import utils.Constants;
+
+public class Subscriber implements Runnable {
+    
+    private Boolean exit = false;
+    private ManagedSubscription subscription;
+    private OpcUaClient opcUaClient;
+    private static Integer namespace =3;
+    private static String nodeId = "AirConditioner_1.Temperature";
+ 
+    //Il tempo (espresso in ms) che passa prima che il subscriber richieda eventuali aggiornamenti al server
+    private Integer publishingInterval= 1;
+    
+    //Il tempo (espresso in ms) che passa prima che il server controlli eventuali cambiamenti dell'elemento da monitorare
+    private Double samplingInterval= 1.0;
+    
+    //La dimensione della coda rappresenta quanti cambiamenti di valore possono essere salvati prima di applicare una policy di discard
+    private Integer queueSize= 5;
+    
+    //La policy di discard da applicare
+    private Boolean discardOldest=true;
+    
+    //Se settato a REPORTING il server campiona il monitoredItem e invia i cambiamenti passato l'intervallo di publishing,
+    //se settato a SAMPLING il server campiona il monitoredItem ma non invia i cambiamenti
+    //se settato a DISABLED il server non effettua alcuna operazione
+    private MonitoringMode monitoringMode=MonitoringMode.Reporting;
+    
+    public Subscriber(OpcUaClient opcUaClient) throws UaException {
+	this.opcUaClient=opcUaClient;
+	
+	//Creiamo la configurazione necessaria al monitoraggio
+	createConfiguration(opcUaClient);
+    } 
+    
+    private void createConfiguration(OpcUaClient opcUaClient) throws UaException {
+	
+	//Creiamo il punto di accesso per monitorare gli elementi e la creazione delle sottoscrizioni
+	//il secondo parametro è opzionale ed indica quanto tempo bisogna attendere per richiedere l'aggiornamento dell'elemento
+	subscription = ManagedSubscription.create(opcUaClient, publishingInterval);
+	
+	//Aggiungiamo al gestore di sottoscrizioni l'elemento da monitorare
+	ManagedDataItem dataItem = subscription.createDataItem(new NodeId(namespace,nodeId ));
+	dataItem.setSamplingInterval(samplingInterval);
+	dataItem.setQueueSize(UInteger.valueOf(queueSize));
+	dataItem.setDiscardOldest(discardOldest);
+	dataItem.setMonitoringMode(monitoringMode);
+	
+	//Aggiungiamo al gestore di sottoscrizioni le reazioni a determinati eventi
+	subscription.addChangeListener(new ChangeListener() {
+	    	
+	    	//Quando l'elemento (o gli elementi) da monitorare cambiano e passa il tempo di pulling
+	    	//stampiamo il valore aggiornato
+    	        @Override
+    	        public void onDataReceived(List<ManagedDataItem> dataItems, List<DataValue> dataValues) {
+    	            System.out.println("->"+dataValues.get(0).getValue().getValue());           
+    	            if(dataValues.get(0).getValue().getValue().equals(-1)) {
+    	        	exit = true;
+    	            }
+    	        }
+    	        
+    	    });
+    }
+
+
+    public static void main(String[] args) throws UaException, InterruptedException, ExecutionException {
+	final String endpoint = String.format("opc.tcp://%s:%s%s", "opcuaserver.com", 48010, "/test");
+	
+	//Creiamo una connessione sincrona con il server OPC
+	OpcUaClient opcUaClient = SynchronousClient.connect(endpoint);
+	
+	//Viene istanziato il thread e fatto partire
+	Subscriber subscriptionThread =new Subscriber(opcUaClient);
+	subscriptionThread.run();
+	
+    }
+
+    private void delete() throws UaException {
+	
+	//Chiudiamo tutte le sottoscrizioni, altrimenti il server continuerà a restituire i cambiamenti
+	subscription.delete();
+	opcUaClient.disconnect();
+    }
+
+    @Override
+    public void run() {
+	while(!exit) {
+	    try {
+		Thread.sleep(1000);
+	    } catch (InterruptedException e) {
+		e.printStackTrace();
+	    }
+	}
+	try {
+	    delete();
+	} catch (UaException e) {
+	    e.printStackTrace();
+	}
+    }
+}
