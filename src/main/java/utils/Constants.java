ee0e7be061e3174ccdb3a589c4f643ce795a010b
giovanni.iannuzzi <giovanni.iannuzzi@OCTANS01.tezar.bogus>
2020-12-03 12:47:46 +0100

    Sterfive

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -5,9 +5,11 @@ public final class Constants {
     	
     }
 
-    public static final String HOST = "localhost"; 		//localhost						
-    							        //milo.digitalpetri.com
-    public static final int PORT = Integer.getInteger("port", 12686); //12686 (localhost)
-    								      //62541 (petri)
+    public static final String HOST = "localhost"; 		//localhost													        //milo.digitalpetri.com
+    public static final int PORT = Integer.getInteger("port", 12686); //12686 (localhost)								      //62541 (petri)
     public static final String PATH = "/milo";
+    
+    public static final String HOSTSTERFIVE = "opcuademo.sterfive.com";
+    public static final int PORTSTERFIVE = Integer.getInteger("port", 26543);
+    public static final String PATHSTERFIVE = "";
 }
