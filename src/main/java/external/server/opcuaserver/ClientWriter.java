554e220e17c0b56f20214f6122c04502f92e22bf
Giovanni <Giovanni@192.168.114.15>
2020-12-04 10:41:24 +0100

    Output nodo AirConditioner_1 e cambio di temperatura su AirConditioner_1.TemperatureSetPoint

---
+++ src/main/java/external/server/opcuaserver/ClientWriter.java
@@ -0,0 +1,46 @@
+package external.server.opcuaserver;
+
+import java.util.concurrent.ExecutionException;
+
+import org.eclipse.milo.opcua.sdk.client.AddressSpace;
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.nodes.UaVariableNode;
+import org.eclipse.milo.opcua.stack.core.UaException;
+import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
+
+import client.SynchronousClient;
+import utils.Constants;
+
+public class ClientWriter {
+    
+    private static Integer namespace =3;
+    private static String nodeId = "AirConditioner_1.TemperatureSetPoint";
+    
+    public static void main(String[] args) throws UaException, InterruptedException, ExecutionException  {
+	final String endpoint = String.format("opc.tcp://%s:%s%s", "opcuaserver.com", 48010, "/test");
+	
+	//Creiamo una connessione sincrona con il server OPC
+	OpcUaClient opcUaClient = SynchronousClient.connect(endpoint);
+	
+	//Ricaviamo l'address space che contiene tutti i nodi del server
+	AddressSpace addressSpace = opcUaClient.getAddressSpace();
+	
+	//Prendiamo il nodo variabile attraverso l'utilizzo di namespace e id
+	UaVariableNode testNode = (UaVariableNode) addressSpace.getNode(
+		    new NodeId(namespace, nodeId)
+		);
+	
+	//Stampiamo il valore del nodo prima della scrittura
+	System.out.println(testNode.readValue().getValue().getValue());
+	
+	//Scriviamo il valore del nodo variabile (in caso di errori viene sollevata un'eccezione)
+	testNode.writeValue(new Variant(0));
+	
+	//Stampiamo il valore del nodo dopo la scrittura
+	System.out.println(testNode.readValue().getValue().getValue());
+	
+	opcUaClient.disconnect();
+    }
+
+}
